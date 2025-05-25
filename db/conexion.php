0c361952d5c6b792d2fed8d635a58b38ca459c0d
Maylo Daring Parra Aguirre <162264699+Mayloparra24@users.noreply.github.com>
2025-05-24 19:04:43 -0600

    actualización de la conexion

--- db/conexion.php
+++ db/conexion.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $dbname = "auroraboutique";
-$user = "postgres";
-$password = "tu_contraseña";
+$user = "joomlauser";
+$password = "Linux20!";
 
 try {
     $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
